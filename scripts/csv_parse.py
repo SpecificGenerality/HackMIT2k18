53f75bdf4f42cd0485a427177107ce0410ce0ece
SpecificGenerality <justiny@princeton.edu>
2018-09-15 23:19:31 -0400

    fill "safe" states with zeroes

--- scripts/csv_parse.py
+++ scripts/csv_parse.py
@@ -7,6 +7,8 @@ goal_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data')
 DATA_URL = os.path.abspath(goal_dir)
 data_path = os.path.join(DATA_URL, "DisasterDeclarationsSummaries.csv")
 
+states = []
+
 # parse the CSV into a dict
 with open(data_path, mode='r') as csv_file:
     csv_reader = pd.read_csv(
@@ -45,6 +47,8 @@ with open(data_path, mode='r') as csv_file:
     # fill big_cols with columns by disaster type
     for year, states in big_dict.items():
         for state, disasters in states.items():
+            if state not in states:
+                states.append[state]
             for disaster, freq in disasters.items():
                 if disaster not in big_cols.keys():
                     big_cols[disaster] = []
@@ -75,6 +79,15 @@ with open(data_path, mode='r') as csv_file:
                     cols[disaster].append(int(disasters[disaster]))
                 else:
                     cols[disaster].append(0)
+        for state in states:
+            if state not in cols['state']:
+                for disaster in cols:
+                    if disaster == 'state':
+                        cols['state'].append(state)
+                    elif disaster == 'fyDeclared':
+                        cols['fyDeclared'].append(int(k))
+                    else:
+                        cols[disaster].append(0)
 
         for c, l in big_cols.items():
             l.extend(cols[c])
