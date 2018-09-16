5e37ef5b58b53d6fd9edc60068c19038d7b4d208
dfan97 <davidfan219@gmail.com>
2018-09-16 01:23:19 -0400

    Two columns

--- app.py
+++ app.py
@@ -54,65 +54,130 @@ app.layout = html.Div(children=[
   '''),
 
   html.Div([
-    dcc.Slider(
-      id='slider-updatemode',
-      marks={i: '{}'.format(i) for i in range(1990, 2018, 1)},
-      min=1990,
-      max=2018,
-      step=1,
-      value=2010,
-      updatemode='drag'
+  html.Div([html.H3('Plot 1'),
+    html.Div([
+      dcc.Slider(
+        id='slider-updatemode',
+        marks={i: '{}'.format(i) for i in range(2000, 2019, 1)},
+        min=2000,
+        max=2018,
+        step=1,
+        value=2010,
+        updatemode='drag'
+      ),
+    ], style={'margin':15}),
+
+    html.Div(id='updatemode-output-container', style={'margin-top': 30}),
+
+    # Dropdown values correspond to the CSV column name
+    dcc.Dropdown(
+      id='disaster-dropdown',
+      options=[
+        {'label': 'Earthquakes', 'value': 'Earthquake'},
+        {'label': 'Flood', 'value': 'Flood'},
+        {'label': 'Hurricanes', 'value': 'Hurricane'},
+        {'label': 'Forest Fires', 'value': 'Fire'},
+        {'label': 'Snow', 'value': 'Snow'}
+      ],
+      value='Earthquake'
     ),
-  ], style={'margin':15}),
-
-  html.Div(id='updatemode-output-container', style={'margin-top': 30}),
-
-  # Dropdown values correspond to the CSV column name
-  dcc.Dropdown(
-    id='disaster-dropdown',
-    options=[
-      {'label': 'Earthquakes', 'value': 'Earthquake'},
-      {'label': 'Flood', 'value': 'Flood'},
-      {'label': 'Hurricanes', 'value': 'Hurricane'},
-      {'label': 'Forest Fires', 'value': 'Fire'},
-      {'label': 'Snow', 'value': 'Snow'}
-    ],
-    value='Earthquake'
-  ),
-
-  dcc.Graph(
-    id = 'state-choropleth',
-    figure = dict(
-      data = [dict(
-        type='choropleth',
-        colorscale = scl,
-        autocolorscale = True,
-        locations = df['state'],
-        z = df['Earthquake'].astype(float),
-        locationmode = 'USA-states',
-        text = df['text'],
-        marker = dict(
-          line = dict (
-            color = 'rgb(255,255,255)',
-            width = 2
-          )),
-        colorbar = dict(title = "Earthquake")
-      )],
-      layout = dict(
-        title = '2010 Earthquakes by State',
-        geo = dict(
-          scope='usa',
-          projection=dict( type='albers usa' ),
-          showlakes = True,
-          lakecolor = 'rgb(255, 255, 255)'
-        ),
+    dcc.Graph(
+      id = 'state-choropleth',
+      figure = dict(
+        data = [dict(
+          type='choropleth',
+          colorscale = scl,
+          autocolorscale = True,
+          locations = df['state'],
+          z = df['Earthquake'].astype(float),
+          locationmode = 'USA-states',
+          text = df['text'],
+          marker = dict(
+            line = dict (
+              color = 'rgb(255,255,255)',
+              width = 2
+            )),
+          colorbar = dict(title = "Earthquake")
+        )],
+        layout = dict(
+          title = '2010 Earthquakes by State',
+          geo = dict(
+            scope='usa',
+            projection=dict( type='albers usa' ),
+            showlakes = True,
+            lakecolor = 'rgb(255, 255, 255)'
+          ),
+        )
       )
-    )
-  )
+    )], className="six columns"),
+
+    html.Div([html.H3('Plot 2'),
+      html.Div([
+        dcc.Slider(
+          id='slider-updatemode2',
+          marks={i: '{}'.format(i) for i in range(2000, 2019, 1)},
+          min=2000,
+          max=2018,
+          step=1,
+          value=2010,
+          updatemode='drag'
+        ),
+      ], style={'margin':15}),
+
+      html.Div(id='updatemode-output-container2', style={'margin-top': 30}),
+
+      # Dropdown values correspond to the CSV column name
+      dcc.Dropdown(
+        id='disaster-dropdown2',
+        options=[
+          {'label': 'Earthquakes', 'value': 'Earthquake'},
+          {'label': 'Flood', 'value': 'Flood'},
+          {'label': 'Hurricanes', 'value': 'Hurricane'},
+          {'label': 'Forest Fires', 'value': 'Fire'},
+          {'label': 'Snow', 'value': 'Snow'}
+        ],
+        value='Earthquake'
+      ),
+      dcc.Graph(
+        id = 'state-choropleth2',
+        figure = dict(
+          data = [dict(
+            type='choropleth',
+            colorscale = scl,
+            autocolorscale = True,
+            locations = df['state'],
+            z = df['Earthquake'].astype(float),
+            locationmode = 'USA-states',
+            text = df['text'],
+            marker = dict(
+              line = dict (
+                color = 'rgb(255,255,255)',
+                width = 2
+              )),
+            colorbar = dict(title = "Earthquake")
+          )],
+          layout = dict(
+            title = '2010 Earthquakes by State',
+            geo = dict(
+              scope='usa',
+              projection=dict( type='albers usa' ),
+              showlakes = True,
+              lakecolor = 'rgb(255, 255, 255)'
+            ),
+          )
+        )
+      )], className="six columns"),
+      ])
 ])
 
 @app.callback(Output('state-choropleth', 'figure'),
               [Input('disaster-dropdown', 'value'), Input('slider-updatemode', 'value')])
+def update_graph(disaster, year):
+  dff = df[df['fyDeclared'] == str(year)]
+  return plotMap(dff, year, disaster)
+
+@app.callback(Output('state-choropleth2', 'figure'),
+              [Input('disaster-dropdown2', 'value'), Input('slider-updatemode2', 'value')])
 def update_graph(disaster, year):
   dff = df[df['fyDeclared'] == str(year)]
   return plotMap(dff, year, disaster)
