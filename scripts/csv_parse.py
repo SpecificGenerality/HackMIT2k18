023d7783dd3b87f0e47cb623f927b82bacfdc3a4
SpecificGenerality <justiny@princeton.edu>
2018-09-15 21:12:30 -0400

    csv_parse v2.0

--- scripts/csv_parse.py
+++ scripts/csv_parse.py
@@ -1,7 +1,7 @@
 # for the csv wrangling
-import csv
 import sys
 import os
+import pandas as pd
 from urllib.parse import urljoin
 
 goal_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data')
@@ -10,13 +10,14 @@ data_path = os.path.join(DATA_URL, "DisasterDeclarationsSummaries.csv")
 
 # parse the CSV into a dict
 with open(data_path, mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+    csv_reader = pd.read_csv(
+        data_path, usecols=['state', 'incidentType', 'fyDeclared'])
     line_count = 0
     big_dict = dict()
-    for row in csv_reader:
+    for index, row in csv_reader.iterrows():
         state = row["state"]
         disaster_type = row["incidentType"]
-        year = int(row["declarationDate"].split("-")[0])
+        year = int(row["fyDeclared"])
 
         # read all of the data into big_dict
         # example: big_dict = {1953: {'GA':{'Flood' : 10, 'Hurricane': 1}}}
@@ -39,13 +40,25 @@ with open(data_path, mode='r') as csv_file:
             by_state[disaster_type] = by_state[disaster_type] + 1
 
     for k, v in big_dict.items():
-        file_name = os.path.join(DATA_URL, str(k) + ("disasters.csv"))
-        with open(file_name, 'w+', newline='') as csv_file:
-            csv_writer = csv.writer(
-                csv_file,
-                delimiter=',',
-                quotechar='"',
-                quoting=csv.QUOTE_MINIMAL)
-            for qee, walue in v.items():
-                for key, value in walue.items():
-                    csv_writer.writerow([qee] + [key] + [str(value)])
+        dir_out = os.path.join(goal_dir, str(k) + "disasters.csv")
+
+        cols = dict()
+        cols['state'] = []
+        for k, v in big_dict.items():
+            for key, value in v.items():
+                for qee, walue in value.items():
+                    if qee not in cols.keys():
+                        cols[qee] = []
+
+        for key, value in v.items():
+            cols['state'].append(key)
+            for disaster in cols:
+                if disaster == 'state':
+                    continue
+                if disaster in value.keys():
+                    cols[disaster].append(int(value[disaster]))
+                else:
+                    cols[disaster].append(0)
+
+        df = pd.DataFrame(data=cols)
+        df.to_csv(dir_out)
